9ef65061bb5ac84934887e74e3aecfac385e3223
zhenzhang24 <zhang.zhen2@gmail.com>
2016-12-04 12:46:35 -0800

    some clean up

--- lr/xor.py
+++ lr/xor.py
@@ -17,7 +17,6 @@ class XOR:
     def arr_not(self, arr, col):
         arr[:,col] = 1 - arr[:,col]
         return arr
-        #return map(lambda x: 1-x, arr)
 
     def forward(self, X):
         x_not_y = np.array(X)
@@ -26,9 +25,7 @@ class XOR:
         not_x_y = self.arr_not(not_x_y, 0)
 
         self.neuron_and1.forward(x_not_y)
-        #self.neuron_and1.debug()
         self.neuron_and2.forward(not_x_y)
-        #self.neuron_and2.debug()
 
 
         l1_out_1 = self.neuron_and1.Y_prob
@@ -36,16 +33,11 @@ class XOR:
 
         l2_in = np.concatenate((l1_out_1, l1_out_2), axis=1)
         or_output = self.neuron_or.forward(l2_in)
-        #self.neuron_or.debug()
-
 
     def backward(self, Y):
         self.neuron_or.accu_errors = Y - self.neuron_or.Y_prob
         
-        #self.neuron_or.debug()
         self.neuron_or.calc_node_delta()
-        #print "or node delta"
-        #print self.neuron_or.node_delta
         self.neuron_or.calc_backward_error()
 
         self.back_propagate(self.neuron_and1, 
