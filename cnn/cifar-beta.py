62febc8940b6c265bedb0facfee9d2bce59879cb
zhenzhang24 <zhang.zhen2@gmail.com>
2017-03-14 16:32:25 -0700

    add nin and googlenet

--- cnn/cifar-beta.py
+++ cnn/cifar-beta.py
@@ -1,10 +1,11 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
+import random
 import input_data
 
 class ConvLayer:
-    def __init__(self, num_input_fm, num_filter, filter_shape, stride):
+    def __init__(self, num_input_fm, num_filter, filter_shape, stride, dropout):
         self.num_input_feature_map = num_input_fm
         self.num_filter = num_filter
         self.filter_shape = filter_shape
@@ -14,6 +15,7 @@ class ConvLayer:
             stddev = 0.1))
         self.biases = tf.Variable(tf.constant(0.1, shape=[num_filter]))
         self.stride = stride
+        self.dropout = dropout
 
     # input shape [batch, in_height, in_width, in_channels] and 
     # a filter shape [filter_height, filter_width, 
@@ -21,21 +23,14 @@ class ConvLayer:
     def conv(self, input_fm):
         return tf.nn.relu(tf.nn.conv2d(input_fm, self.W, 
                                     strides=[1, self.stride, self.stride, 1], 
-                                    padding='SAME')
+                                    padding='VALID')
                         + self.biases)
 
-    def lrn(self, relu_res):
-        radius = 2; alpha = 2e-05; beta = 0.75; bias = 1.0
-        lrn_res = tf.nn.local_response_normalization(relu_res, 
-                depth_radius=radius, 
-                alpha=alpha, 
-                beta=beta, 
-                bias=bias)
-        return lrn_res
-        
+       
     def compute(self, input_fm):
         conv_res = self.conv(input_fm)
-        return self.lrn(conv_res)
+        conv_res = tf.nn.dropout(conv_res, self.dropout)
+        return conv_res
 
 class MaxPoolLayer:
     def __init__(self, ksize, stride):
@@ -52,6 +47,19 @@ class MaxPoolLayer:
     def compute(self, input_fm):
         return self.pool(input_fm)
 
+class LRN: 
+    def __init__(self):
+        pass
+
+    def compute(self, relu_res):
+        radius = 2; alpha = 2e-05; beta = 0.75; bias = 1.0
+        lrn_res = tf.nn.local_response_normalization(relu_res, 
+                depth_radius=radius, 
+                alpha=alpha, 
+                beta=beta, 
+                bias=bias)
+        return lrn_res
+ 
 class FCLayer:
     def __init__(self, input_size, out_features, relu=True):
         self.input_size = input_size
@@ -94,27 +102,50 @@ class CNNNet:
 def initiate_layer(layer):
     if layer['type'] == 'conv':
         (in_channels, out_channels, ksize, strides)= layer['config']
-        res = ConvLayer(in_channels, out_channels, (ksize, ksize), strides)
+        if layer.has_key('dropout'):
+            dropout = layer['dropout']
+        else:
+            dropout = 1
+        res = ConvLayer(in_channels, out_channels, 
+                (ksize, ksize), strides, dropout)
         return res
     if layer['type'] == 'pool':
         (ksize, strides) = layer['config']
         return MaxPoolLayer(ksize, strides)
 
+    if layer['type'] == 'lrn':
+        return LRN()
+
 cur_index = 0
-def generate_next_batch(batch_size):
+ids = []
+def generate_next_batch2(batch_size):
     global cur_index, train_input, train_output
+    ids = random.sample(range(len(train_input)), batch_size)
+    batch_x = []
+    batch_y = []
+    for id in ids:
+        batch_x.append(train_input[id])
+        batch_y.append(train_output[id])
+    return np.array(batch_x), np.array(batch_y)
+
+def generate_next_batch(batch_size):
+    global cur_index, train_input, train_output, ids
     data_size = len(train_input)
+    if len(ids) == 0:
+        ids = range(data_size)
     end_index = (cur_index + batch_size) % data_size
     if end_index < cur_index:
-        batch_x = np.concatenate(
-                (train_input[cur_index:], train_input[:end_index]))
-        batch_y = np.concatenate(
-                (train_output[cur_index:], train_output[:end_index]))
+        batch_ids = ids[cur_index:] + ids[:end_index]
+   #     random.shuffle(ids)
     else:
-        batch_x = train_input[cur_index:end_index]
-        batch_y = train_output[cur_index:end_index]
-        cur_index = end_index
-    return batch_x, batch_y
+        batch_ids = ids[cur_index:end_index]
+    cur_index = end_index
+    batch_x = []
+    batch_y = []
+    for id in batch_ids:
+        batch_x.append(train_input[id])
+        batch_y.append(train_output[id])
+    return np.array(batch_x), np.array(batch_y)
 
 def evaluate(output_logit, obsv):
     pred = tf.argmax(output_logit, 1)
@@ -128,44 +159,54 @@ image_height = 32
 initial_num_features = 3
 learning_rate = 0.001
 batch_size = 50
-rounds = 200
+rounds = 20000
 output_dimension = 10
 
-train_input, train_output = input_data.read_data_sets(
-    "/Users/zhenzhang/Documents/ML/data/cifar-10/data_batch_1")
+#train_input, train_output = input_data.read_data_sets(
+#    "/Users/zhenzhang/Documents/ML/data/cifar-10/data_batch_1")
 
+test_input, test_output = input_data.read_data_sets(
+    "/Users/zhenzhang/Documents/ML/data/cifar-10/test_batch")
 x = tf.placeholder(tf.float32, 
         shape=(None, image_height, image_width, initial_num_features))
 y_ = tf.placeholder(tf.int64, shape=(None))
+dropout = tf.placeholder(tf.float32)
 
 configs = []
-configs.append({'type':'conv', 'config':(3, 32, 3, 1)})
-#configs.append({'type':'conv', 'config':(32, 32, 3, 1)})
+configs.append({'type':'conv', 'config':(3, 32, 2, 1), 'dropout':dropout})
 configs.append({'type':'pool', 'config':(3, 2)})
+configs.append({'type':'lrn'})
 
-configs.append({'type':'conv', 'config':(32, 64, 3, 1)})
-#configs.append({'type':'conv', 'config':(64, 64, 3, 1)})
+configs.append({'type':'conv', 'config':(32, 64, 2, 1), 'dropout':dropout})
 configs.append({'type':'pool', 'config':(3, 2)})
+configs.append({'type':'lrn'})
 
-configs.append({'type':'conv', 'config':(64, 128, 3, 1)})
+configs.append({'type':'conv', 'config':(64, 64, 2, 1), 'dropout':dropout})
 configs.append({'type':'pool', 'config':(3, 1)})
+configs.append({'type':'lrn'})
+
+configs.append({'type':'conv', 'config':(64, 64, 2, 1), 'dropout':dropout})
+configs.append({'type':'pool', 'config':(3, 1)})
+configs.append({'type':'lrn'})
+
+configs.append({'type':'conv', 'config':(64, 128, 2, 1)})
+configs.append({'type':'pool', 'config':(3, 1)})
+configs.append({'type':'lrn'})
 
 network = CNNNet(configs)
 (cnn_output, cnn_structure) = network.forward(x)
 
-fc_1_num_features = 256
+fc_1_num_features = 512
 shape = cnn_output.get_shape()
 fc_1_input_shape = int(np.prod(shape[1:]))
 fc_1 = FCLayer(fc_1_input_shape, fc_1_num_features).matmul(cnn_output)
 
-fc_2_num_features = 256
+fc_2_num_features = 1024
 fc_2 = FCLayer(fc_1_num_features, fc_2_num_features).matmul(fc_1)
-
 
 fc_output_num_features = output_dimension
 fc_output = FCLayer(fc_2_num_features, fc_output_num_features, False).matmul(fc_2)
 
-keep_prob = tf.placeholder(tf.float32)
 pred_logits = fc_output
 
 loss = tf.nn.sparse_softmax_cross_entropy_with_logits(pred_logits, y_) 
@@ -179,21 +220,29 @@ sess.run(init)
 num_examples = 10000
 steps = num_examples / batch_size
 print configs
-for r in range(rounds):
-    for i in range(steps):
+fno = 0
+for i in range(rounds):
+        if i % 1000 == 0:
+            fno = (fno) % 5 + 1
+            filename = "/Users/zhenzhang/Documents/ML/data/cifar-10/data_batch_"+str(fno)
+            print filename
+            train_input, train_output = input_data.read_data_sets(
+                    filename)
         batch_x, batch_y = generate_next_batch(batch_size)
         _, x_in, output, accuracy = sess.run(
                 [trainer, x, pred_logits, evaluator],
-                feed_dict={x:batch_x, y_:batch_y, keep_prob:0.5})
-        if i % 100 == 0:
+                feed_dict={x:batch_x, y_:batch_y, dropout:0.8})
+        if i == 0:
             x_in, output, structure= sess.run(
                     [x, pred_logits, cnn_structure],
-                    feed_dict={x:batch_x, y_:batch_y, keep_prob:0.5})
+                    feed_dict={x:batch_x, y_:batch_y, dropout:0.8})
             print "input shape", x_in.shape
             print "output shape", output.shape
             for l in structure:
                 print l
 
+        if i % 100 == 0:
             accuracy = sess.run(evaluator, 
-                feed_dict={x:train_input, y_:train_output, keep_prob:0.5})
-            print "evaluating accuracy %d steps: %f" % (r*steps + i, accuracy)
+                    feed_dict={x:test_input[:400], y_:test_output[:400], dropout:1.0})
+            print "evaluating accuracy %d steps: %f" % (i, accuracy)
+
